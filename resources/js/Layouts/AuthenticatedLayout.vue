d6bba7e9a4793d2a0e1f60998074cc80e6726ecb
Jasa-Hollow <joel2262000@gmail.com>
2023-04-09 11:16:12 -0600

    Dashboard and Sidebar develop

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -13,8 +13,7 @@
 
 <template>
     <div class="min-h-screen bg-gray-100">
-        <nav class="bg-[#6C3B2A] border-b border-gray-100">
-
+        <nav class="bg-brown-500 border-b border-gray-100">
             <!-- Primary Navigation Menu -->
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-12">
--- resources/js/Layouts/SideBar.vue
+++ resources/js/Layouts/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <p>Hola Mundo</p>
+        <p>-</p>
     </div>
 </template>
