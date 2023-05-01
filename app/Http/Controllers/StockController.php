3a1bb88aa2db569c0f0992bccd2d586d93555c59
cristian mejia <39344286+mejiadev643@users.noreply.github.com>
2023-05-01 11:44:03 -0600

    agregado el permiso de editar para el stock

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Stock;
 use App\Http\Resources\ProductCollection;
+use App\Http\Resources\StockCollection;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
+
 
 
 
@@ -20,7 +22,7 @@ class StockController extends Controller
             return redirect()->back()->withErrors(['warning' => 'No posees los permisos necesarios. Ponte en contacto con tu manager!.']);
         }*/
 
-        $stock = Stock::join('product','stock.product_id','=','product.id')->select('stock.id','product.name','product.measure','stock.cost','stock.suggested_price','stock.gain')->orderBy('id','desc')->paginate(10);
+        $stock = new StockCollection(Stock::join('product','stock.product_id','=','product.id')->select('stock.id','product.name','product.measure','stock.cost','stock.suggested_price','stock.gain')->orderBy('id','desc')->paginate(10));
         #return $stock;
         $permissions = Auth::user()->getAllPermissions();
 
