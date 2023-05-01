5a6ce922744254fa6930967be21d473b77e3a94b
Álvaro Flores <moralesalvaro308@gamil.com>
2023-05-01 15:28:59 -0600

    chore: Agregando migración entidad TypeDoc

---
+++ database/seeders/TypeDocSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\TypeDoc;
+
+class TypeDocSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TypeDoc::insert([
+            [
+                'name' => 'Ticket',
+                'description' => ''
+            ],
+        ]);
+    }
+}
