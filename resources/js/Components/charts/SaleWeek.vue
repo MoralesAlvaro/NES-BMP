c157a4e91b734dc5f1d2eed08325980e6ab0938d
Álvaro Flores <moralesalvaro308@gamil.com>
2023-05-25 22:05:08 -0600

    Dashboard gráficas

---
+++ resources/js/Components/charts/SaleWeek.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <Bar :options="chartOptions" :data="chartData" />
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: 'SaleWeek',
+    components: { Bar },
+    props: {
+        value: {
+            type: Array,
+            required: true
+        },
+        chartTitle: {
+            type: String,
+            default: 'Bar Chart'
+        },
+        width: {
+            type: Number,
+            default: 1500
+        },
+        height: {
+            type: Number,
+            default: 1500
+        },
+    },
+
+
+    data() {
+        return {
+            chartData: {
+                labels: [`Semana ${this.value.map(item => item.week)}`],
+                datasets: [{
+                    label: 'Número de Ventas',
+                    backgroundColor: '#DB763B',
+                    borderColor: '#d1d5db',
+                    fill: false,
+                    lineTension: 0,
+                    data: this.value.map(item => item.totales)
+                }]
+            },
+            chartOptions: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    display: false,
+                },
+                scales: {},
+                tooltips: {
+                    mode: 'label'
+                },
+                // Agrega las propiedades width y height con valor '100%'
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                width: '100%',
+                height: '100%'
+            }
+
+        }
+    }
+}
+</script>
--- resources/js/Pages/Dashboard/Show.vue
+++ resources/js/Pages/Dashboard/Show.vue
@@ -4,42 +4,76 @@ import AppLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import Table from '@/Components/Table.vue';
 import Button from '@/Components/PrimaryButton.vue';
+import LineChart from '@/Components/charts/LineChart.vue'
+import SaleWek from '@/Components/charts/SaleWeek.vue'
+import { reactive, ref } from 'vue';
+
 
 const props = defineProps({
-    SaleMonth: Object,
-    Top3: Object,
-    SaleWeek: Object
+    SaleMonth: Array,
+    Top3: Array,
+    SaleWeek: Array
 });
+
+const header = reactive([{
+    name: 'Producto',
+    showInMobile: true
+},
+{
+    name: 'Cantidad',
+    showInMobile: true
+}
+]);
 
 </script>
 
 
 <template>
-    <Head title="Dashboard" />
-    <AppLayout>
-        <!-- Encabezado del componente -->
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-700 leading-tight sm:text-end md:text-start text-start">
-                Dashboard
-            </h2>
-        </template>
+    <div>
 
-        <div class="py-6 grid md:grid-cols-12 sm:grid-cols-1 gap-1">
+        <Head title="Dashboard" />
+        <AppLayout>
+            <!-- Encabezado del componente -->
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-700 leading-tight sm:text-end md:text-start text-start">
+                    Dashboard
+                </h2>
+            </template>
 
-            <div
-                class="md:col-span-6 md:ml-7 md:mr-5 md:min-h-screen sm:h-auto flex flex-col sm:justify-start items-center pt-6 sm:pt-0">
-                <div class="mt-6 px-6 py-4 bg-white shadow-lg overflow-hidden sm:rounded-lg ">
-                    
+            <div class="py-6 grid md:grid-cols-12 sm:grid-cols-1 gap-1">
+
+                <div class="md:col-span-6 md:ml-7 h-auto md:mr-5 flex flex-col sm:justify-start items-center pt-6 sm:pt-0">
+                    <div
+                        class="w-full h-80 bg-white shadow-lg overflow-hidden sm:rounded-lg flex flex-col items-center justify-start">
+                        <span class="text-gray-500 font-bold text-center mt-2">Ventas Semanales</span>
+                        <SaleWek class="w-full h-full p-2" :value="SaleWeek" />
+                    </div>
+                </div>
+
+                <div class="md:col-span-6 md:ml-7 md:mr-5 flex flex-col sm:justify-start items-center pt-6 sm:pt-0">
+                    <div
+                        class="w-full h-80 bg-white shadow-lg overflow-hidden sm:rounded-lg flex flex-col items-center justify-start">
+                        <span class="text-gray-500 font-bold text-center mt-2">Ventas Mensulaes</span>
+                        <LineChart class="w-full h-full p-2" :value="SaleMonth" />
+                    </div>
+                </div>
+
+            </div>
+            <div class="md:ml-7 md:mr-5 flex flex-col sm:justify-start items-center pt-6 sm:pt-0">
+                <div class="w-full bg-white shadow-lg overflow-hidden sm:rounded-lg">
+                    <div class="flex justify-center">
+                        <span class="text-gray-500 font-bold text-center mt-2">Top productos más vendidos</span>
+                    </div>
+                    <Table :header="header" :items="Top3.length">
+                        <tbody class="px-5">
+                            <tr v-for="item in Top3" class="mt-2">
+                                <td class="text-center p-2 lg:text-base text-xs capitalize">{{ item.name }}</td>
+                                <td class="text-center p-2 lg:text-base text-xs">{{ item.totales }}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </div>
             </div>
-
-            <div
-                class="md:col-span-6 md:ml-7 md:mr-5 md:min-h-screen sm:h-auto flex flex-col sm:justify-start items-center pt-6 sm:pt-0">
-                <div class="mt-6 px-6 py-4 bg-white shadow-lg overflow-hidden sm:rounded-lg">
-                    
-                </div>
-            </div>
-
-        </div>
-    </AppLayout>
+        </AppLayout>
+    </div>
 </template>
